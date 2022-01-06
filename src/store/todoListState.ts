8382a224b07144de1fc6b659a5ca9de37b516eb9
hito-kotaro <tohi.work@gmail.com>
2022-01-07 08:48:37 +0900

    グローバルステート実装

--- src/store/todoListState.ts
+++ src/store/todoListState.ts
@@ -1,6 +1,5 @@
 import { atom } from 'recoil';
 
 export const todoListState = atom({ key: 'TODO_LIST_STATE', default: [] });
-// export const isFirst = atom({ key: 'IS_FIRST', default: true });
 
 export default todoListState;
